2e5e394e1490ee80baa2e3b8ea2c0bed99602d5f
udinantonn@gmail.com <vagrant@ubuntu2010.localdomain>
2023-08-28 09:12:07 +0000

    jenkinsfile_fix#1

--- jenkins/casc_configs/seedjob.groovy
+++ jenkins/casc_configs/seedjob.groovy
@@ -1,4 +1,4 @@
-pipelineJob('nginx_deploy') {
+pipelineJob('nginx_main') {
     definition {
         cpsScm {
             scm {
@@ -10,25 +10,30 @@ pipelineJob('nginx_deploy') {
                     branch('main')
                 }
             }
-            scriptPath('Jenkinsfile')
+            triggers {
+                scm('*/15 * * * *')
+            }
+            scriptPath('Jenkinsfile_main')
         }
     }
 } 
 
-multibranchPipelineJob('main_and_test') {
-    branchSources {
-        github {
-            id('23232323') // IMPORTANT: use a constant and unique identifier
-            scanCredentialsId('github_PAT')
-            repoOwner('2Core3')
-            repository('Diplom_nginx.git')    
-            triggers {
-                urlTrigger {
-                    url('https://github.com/2Core3/Diplom_nginx.git') {
-                        check('status')
+pipelineJob('nginx_test') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('2Core3/Diplom_nginx', 'ssh')
+                        credentials('github_ssh')
                     }
+                    branch('test')
                 }
             }
+            triggers {
+                scm('* * * * *')
+            }
+            scriptPath('Jenkinsfile_test')
         }
     }
-}
+} 
